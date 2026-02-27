b9be557296afa635b511f4b0fe3329f8235dc14e
Pranav Rai <raipranav2987@gmail.com>
2026-02-27 09:58:09 +0530

    prepare for render deployment

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,7 +1,8 @@
 import { useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = import.meta.env.PROD ? '' : 'http://localhost:5000';
+
 
 export const useSocket = (token) => {
     const socketRef = useRef(null);
